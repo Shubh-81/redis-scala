724b31586fbcbb2d0c0a6e2895e1d3c0684c6612
Shubh-81 <shubh57ag@gmail.com>
2025-06-13 13:59:32 +0530

    Added support for multiple request

--- src/main/scala/codecrafters_redis/Server.scala
+++ src/main/scala/codecrafters_redis/Server.scala
@@ -12,10 +12,12 @@ object Server {
     //
     val serverSocket = new ServerSocket()
     serverSocket.bind(new InetSocketAddress("localhost", 6379))
-    val clientSocket = serverSocket.accept() // wait for client
 
-    clientSocket.getOutputStream().write("+PONG\r\n".getBytes());
+    while (true) {
+      val clientSocket = serverSocket.accept() // wait for client
+      clientSocket.getOutputStream().write("+PONG\r\n".getBytes());
 
-    clientSocket.close();
+      clientSocket.close();
+    }    
   }
 }
