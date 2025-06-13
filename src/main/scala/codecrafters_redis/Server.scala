6447c85bbba90bb5af2eed740f0528abe8ef2e48
Shubh-81 <shubh57ag@gmail.com>
2025-06-13 20:06:02 +0530

    Added multi user support using thread

--- src/main/scala/codecrafters_redis/Server.scala
+++ src/main/scala/codecrafters_redis/Server.scala
@@ -3,27 +3,36 @@ package codecrafters_redis
 import java.net._;
 import java.io._;
 import scala.util.Using
+import scala.collection.mutable.ListBuffer
 
 object Server {
-  def main(args: Array[String]): Unit = {
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    println("Logs from your program will appear here!")
 
-    // Uncomment this to pass the first stage
-    //
-    val serverSocket = new ServerSocket()
-    serverSocket.bind(new InetSocketAddress("localhost", 6379))
-    val clientSocket = serverSocket.accept();
+    def main(args: Array[String]): Unit = {
+        val serverSocket = new ServerSocket();
+        serverSocket.bind(new InetSocketAddress("localhost", 6379))
 
-    Using(clientSocket.getInputStream()) { is =>
-      Using (clientSocket.getOutputStream()) { os =>
-        var reader = new BufferedReader(new InputStreamReader(is))
-        reader.lines().forEach { line => 
-          if (line.startsWith("PING")) {
-            os.write("+PONG\r\n".getBytes())
-          }
+        var threads = ListBuffer[Thread]()
+
+        while (true) {
+            val clientSocket = serverSocket.accept()
+
+            val thread = new Thread(() => {         
+                Using(clientSocket.getInputStream()) { is =>
+                    Using(clientSocket.getOutputStream()) { os =>
+                        val reader = new BufferedReader(new InputStreamReader(is))
+
+                        reader.lines().forEach { line =>
+                            if (line.startsWith("PING")) {
+                                os.write("+PONG\r\n".getBytes())
+                            }
+                        }
+                    }
+                }
+            })
+            thread.start()
+            threads += thread
         }
-      }
-    } 
-  }
+        
+        threads.foreach(_.join()) 
+    }
 }
