b7ccd91c48b9a06e59db78b275d4061993ec24de
Shubh-81 <shubh57ag@gmail.com>
2025-06-14 00:03:48 +0530

    Added support for echo command

--- src/main/scala/codecrafters_redis/Server.scala
+++ src/main/scala/codecrafters_redis/Server.scala
@@ -6,16 +6,20 @@ import scala.util.Using
 import scala.collection.mutable.ListBuffer
 import java.util.concurrent.BlockingQueue
 import java.util.concurrent.LinkedBlockingQueue
+import scala.collection.mutable.ArrayBuffer
 
 case class Event(outputStream: OutputStream, message: String)
 
 object Server {
 
     private def processEvent(event: Event): Unit = {
-        if (event.message.startsWith("PING")) {
+        if (event.message.toUpperCase().startsWith("PING")) {
             event.outputStream.write("+PONG\r\n".getBytes())
-            event.outputStream.flush()
+        } else if (event.message.toUpperCase().startsWith("ECHO")) {
+            event.outputStream.write(s"+${event.message.substring(4).trim()}\r\n".getBytes())
         }
+
+        event.outputStream.flush()
     }
 
     private def eventLoop(queue: BlockingQueue[Event]): Unit = {
@@ -54,8 +58,28 @@ object Server {
                 Using.resources(clientSocket.getInputStream(), clientSocket.getOutputStream()) { (is, os) =>
                     val reader = new BufferedReader(new InputStreamReader(is));
                     
+                    var command: ArrayBuffer[String] = ArrayBuffer[String]()
+                    var idx = 0
+                    var len = 0
+
                     reader.lines().forEach { line =>
-                        q.offer(new Event(os, line))
+                        if (line.startsWith("*")) {
+                            len = Integer.parseInt(line.substring(1))
+                            idx = 0
+
+                            command = ArrayBuffer[String]()
+                        } else {
+                            if (idx % 2 == 0) {
+                                idx += 1
+                            } else {
+                                command += line
+                                idx += 1
+
+                                if (idx == 2 * len) {
+                                    q.offer(new Event(os, command.mkString(" ")))
+                                }
+                            }
+                        }
                     }
                 }
             })
